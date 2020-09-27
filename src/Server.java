9f84db76659db552e173c2394f4f5467c51e9477
Anthony Tran-Phan <tonyphan15@gmail.com>
2020-09-26 23:27:02 -0700

    Implement server class

--- src/Server.java
+++ src/Server.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Server {
 	private int port;
-	private Set<String> userNames = new HashSet<>();
+	private Set<String> users = new HashSet<>();
 	private Set<UserThread> userThreads = new HashSet<>();
 	
 	public Server(int port) {
@@ -12,6 +12,37 @@ public class Server {
 	}
 	
 	public void execute() {
-		
+		try(ServerSocket serverSocket = new ServerSocket()) {
+			System.out.println("The server is listening on port " + port);
+			while(true) {
+				Socket socket = serverSocket.accept();
+				System.out.println("New user connected");
+				UserThread newUser = new UserThread(socket, this);
+				userThreads.add(newUser);
+			}
+		} catch(IOException e) {
+			System.out.println("Error in the server: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public void addUsers(String name) {
+		users.add(name);
+	}
+	
+	public void broadcast(String message, UserThread excludeUser) {
+		for(UserThread user : userThreads) {
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		if(args.length < 1) {
+			System.out.println("Syntax: java ChatServer <port-number>");
+			System.exit(0);
+		}
+		int port = Integer.parseInt(args[0]);
+		Server server = new Server(port);
+		server.execute();
 	}
 }
--- src/UserThread.java
+++ src/UserThread.java
@@ -1,4 +1,14 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
-public class UserThread {
-
+public class UserThread extends Thread {
+	private Socket socket;
+	private Server server;
+	private PrintWriter writer;
+	
+	public UserThread(Socket socket, Server server) {
+		this.socket = socket;
+		this.server = server;
+	}
 }
